33274ea7cb66a706ceb9c15ef773a47e56f9b3af
Mike Wähnert <mike.waehnert@kairos.de>
2021-02-04 15:51:13 +0100

    dktk/condition.groovy script fixed

--- src/main/groovy/projects/dktk_with_jpa_navigation/condition.groovy
+++ src/main/groovy/projects/dktk_with_jpa_navigation/condition.groovy
@@ -1,20 +1,13 @@
 package projects.dktk_with_jpa_navigation
 
-import de.kairos.fhir.centraxx.metamodel.AbstractSample
-import de.kairos.fhir.centraxx.metamodel.Diagnosis
-import de.kairos.fhir.centraxx.metamodel.IcdEntry
-import de.kairos.fhir.centraxx.metamodel.RootEntities
-import de.kairos.fhir.centraxx.metamodel.SampleAbstraction
 
+import static de.kairos.fhir.centraxx.metamodel.AbstractEntity.ID
 import static de.kairos.fhir.centraxx.metamodel.RootEntities.diagnosis
-import static de.kairos.fhir.centraxx.metamodel.RootEntities.sample
-import static de.kairos.fhir.centraxx.metamodel.SampleAbstraction.*
 
 /**
  * Represented by a CXX Diagnosis
  * @author Mike Wähnert
- * @since CXX.v.3.17.0.2
- * TODO: Extension Fernmetastasen
+ * @since CXX.v.3.17.0.2* TODO: Extension Fernmetastasen
  */
 condition {
 
@@ -65,12 +58,8 @@ condition {
       system = "http://fhir.de/CodeSystem/dimdi/icd-10-gm"
       code = context.source[diagnosis().icdEntry().code()] as String
       version = context.source[diagnosis().icdEntry().catalogue().catalogueVersion()]
-      display = context.source["icdEntry.name"]
     }
   }
-diagnosis().icdEntry().
-  //TODO: "icdEntry.catalogue.version" catalogueVersion and version the same?
-  //TODO: "icdEntry.name" ?
 
   context.source[diagnosis().samples()]?.each { final sample ->
     extension {
