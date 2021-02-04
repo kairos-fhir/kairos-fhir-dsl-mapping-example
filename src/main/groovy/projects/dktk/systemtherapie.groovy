d5c06d76410589152c8aa0e73d24edcde7b909e7
Mike Wähnert <mike.waehnert@kairos.de>
2021-02-04 17:11:53 +0100

    dktk/systemtherapie.groovy rClassificationLocalDict removed.

--- src/main/groovy/projects/dktk/systemtherapie.groovy
+++ src/main/groovy/projects/dktk/systemtherapie.groovy
@@ -4,6 +4,10 @@ import org.hl7.fhir.r4.model.MedicationStatement
 
 /**
  * Represented by a CXX SystemTherapy
+ *
+ * Hints:
+ * There is no representation in a CXX SystemTherapy for the Extensions StellungZurOp, LokaleResidualstatus and GesamtbeurteilungResidualstatus
+ *
  * @author Mike Wähnert
  * @since CXX.v.3.17.0.4
  */
@@ -35,43 +39,11 @@ medicationStatement {
     reference = "Condition/" + context.source["tumour.centraXXDiagnosis.id"]
   }
 
-  // TODO which CXX field?
-//  extension {
-//    url = "http://dktk.dkfz.de/fhir/StructureDefinition/onco-core-Extension-StellungZurOp"
-//    valueCoding {
-//      system = "http://dktk.dkfz.de/fhir/onco/core/CodeSystem/SYSTStellungOPCS"
-//    }
-//  }
-
   extension {
     url = "http://dktk.dkfz.de/fhir/StructureDefinition/onco-core-Extension-SYSTIntention"
     valueCoding {
       system = "http://dktk.dkfz.de/fhir/onco/core/CodeSystem/SYSTIntentionCS"
       code = context.source["intentionDict"]?.getAt("code")?.toString()?.toUpperCase()
-    }
-  }
-
-  if (context.source["rClassificationLocalDict.code"]) {
-    extension {
-      //TODO Declared is a reference to Observation, but Operation uses a valueCoding
-      url = "http://dktk.dkfz.de/fhir/StructureDefinition/onco-core-Extension-LokaleResidualstatus"
-      valueCoding {
-        system = "http://dktk.dkfz.de/fhir/onco/core/CodeSystem/LokaleBeurteilungResidualstatusCS"
-        version = context.source["rClassificationLocalDict.version"]
-        code = context.source["rClassificationLocalDict.nameMultilingualEntries2"]?.find { it["lang"] == "en" }?.getAt("value")
-      }
-    }
-  }
-
-  if (context.source["rClassificationDict"]) {
-    extension {
-      //TODO Declared is a reference to Observation, but Operation uses a valueCoding
-      url = "http://dktk.dkfz.de/fhir/StructureDefinition/onco-core-Extension-GesamtbeurteilungResidualstatus"
-      valueCoding {
-        system = "http://dktk.dkfz.de/fhir/onco/core/CodeSystem/GesamtbeurteilungResidualstatusCS"
-        version = context.source["rClassificationDict.version"]
-        code = context.source["rClassificationDict.nameMultilingualEntries2"]?.find { it["lang"] == "en" }?.getAt("value")
-      }
     }
   }
 }
