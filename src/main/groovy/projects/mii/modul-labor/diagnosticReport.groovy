cd79c3d818771013a5c1a5a6f23f084d17e92092
kairosjonas <jonas.kuettner@kairos.de>
2021-05-12 19:25:22 +0200

    MII mapping CXX LaborMapping to DiagnosticReport added export of contact data and patient status to patient script.

---
+++ src/main/groovy/projects/mii/modul-labor/diagnosticReport.groovy
@@ -0,0 +1,90 @@
+import de.kairos.fhir.centraxx.metamodel.LaborFindingLaborValue
+import de.kairos.fhir.centraxx.metamodel.LaborValue
+import org.hl7.fhir.r4.model.DiagnosticReport
+
+
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.laborMapping
+
+/**
+ * represented by CXX LaborMapping
+ * @author Jonas Küttner
+ * @since v.1.8.0, CXX.v.3.18.1
+ * TODO: testing. Not tested because export of LaborMapping to DiagnosticReport not yet supported.
+ */
+
+diagnosticReport {
+  id = "DiagnosticReport/" + context.source[laborMapping().laborFinding().id()]
+
+
+  meta {
+    source = "urn:centraxx"
+    profile("https://www.medizininformatik-initiative.de/fhir/core/modul-labor/StructureDefinition/DiagnosticReportLab")
+  }
+
+  identifier {
+    type {
+      coding {
+        system = "http://terminology.hl7.org/CodeSystem/v2-0203"
+        code = "FILL"
+      }
+    }
+    system = "urn:centraxx"
+    value = context.source[laborMapping().laborFinding().laborFindingId()]
+    //TODO: assigner
+  }
+
+  status = DiagnosticReport.DiagnosticReportStatus.UNKNOWN
+
+  category {
+    coding {
+      system = "http://terminology.hl7.org/CodeSystem/v2-0074"
+      code = mapCategories(context.source[laborMapping().laborFinding().laborMethod().category()] as String)
+    }
+  }
+
+  code {
+    coding {
+      system = "http://loinc.org"
+      code = "11502-2"
+    }
+  }
+
+  subject {
+    reference = "Patient/" + context.source[laborMapping().relatedPatient().id()]
+  }
+  encounter {
+    reference = "Encounter/" + context.source[laborMapping().episode().id()]
+  }
+
+  effectiveDateTime {
+    date = normalizeDate(context.source[laborMapping().laborFinding().findingDate()] as String)
+  }
+
+  issued(context.source[laborMapping().laborFinding().creationDate()])
+
+  context.source[laborMapping().laborFinding().laborFindingLaborValues()]?.each {
+    result {
+      reference = it[LaborFindingLaborValue.ID]
+    }
+  }
+
+  final def interpretation = context.source[laborMapping().laborFinding().laborFindingLaborValues()].find {
+    "INTERPRETATION" == it[LaborFindingLaborValue.LABOR_VALUE][LaborValue.CODE]
+  }
+
+
+  conclusion = interpretation ? interpretation[LaborFindingLaborValue.STRING_VALUE] : null
+
+}
+
+static String normalizeDate(final String dateTimeString) {
+  return dateTimeString != null ? dateTimeString.substring(0, 19) : null
+}
+
+static String mapCategories(String cxxCategory) {
+  switch (cxxCategory) {
+    case "LABOR": return "LAB"
+    case "NURSING": return "NRS"
+    default: return "OTH"
+  }
+}
