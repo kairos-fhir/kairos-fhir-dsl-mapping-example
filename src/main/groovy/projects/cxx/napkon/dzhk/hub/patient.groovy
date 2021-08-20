8e39afd038cf68e08e0a5f44a41c156bd3ce883b
Mike Wähnert <mike.waehnert@kairos.de>
2021-08-20 18:03:30 +0200

    script of direction DZHK -> HUB added

---
+++ src/main/groovy/projects/cxx/napkon/dzhk/hub/patient.groovy
@@ -0,0 +1,47 @@
+package projects.cxx.napkon.dzhk.hub
+
+import de.kairos.fhir.centraxx.metamodel.IdContainerType
+
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.ID_CONTAINER_TYPE
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.PSN
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.patientMasterDataAnonymous
+
+/**
+ * Represented by a CXX PatientMasterDataAnonymous
+ * @author Mike Wähnert
+ * @since v.1.5.0, CXX.v.3.17.1.5
+ */
+patient {
+
+  id = "Patient/" + context.source[patientMasterDataAnonymous().patientContainer().id()]
+
+  final def idContainer = context.source[patientMasterDataAnonymous().patientContainer().idContainer()]?.find {
+    "LIMSPSN" == it[ID_CONTAINER_TYPE]?.getAt(IdContainerType.CODE)
+  }
+
+  if (idContainer) {
+    identifier {
+      value = idContainer[PSN]
+      type {
+        coding {
+          system = "urn:centraxx"
+          code = idContainer[ID_CONTAINER_TYPE]?.getAt(IdContainerType.CODE)
+        }
+      }
+    }
+  }
+
+  birthDate = normalizeDate(context.source[patientMasterDataAnonymous().birthdate().date()] as String)
+  deceasedDateTime = "UNKNOWN" != context.source[patientMasterDataAnonymous().dateOfDeath().precision()] ?
+      context.source[patientMasterDataAnonymous().dateOfDeath().date()] : null
+  generalPractitioner {
+    identifier {
+      value = "P-2216-NAP"
+    }
+  }
+
+}
+
+static String normalizeDate(final String dateTimeString) {
+  return dateTimeString != null ? dateTimeString.substring(0, 10) : null // removes the time
+}
