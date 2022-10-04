d9c2af46874fa2e701316f816695e2a7dd4a6da6
Mike Wähnert <mike.waehnert@kairos.de>
2022-10-04 15:25:27 +0200

    izi hannover example scripts: patient script updated

--- src/main/groovy/projects/izi/hannover/patient.groovy
+++ src/main/groovy/projects/izi/hannover/patient.groovy
@@ -1,37 +1,47 @@
 package projects.izi.hannover
 
+
+import de.kairos.fhir.centraxx.metamodel.IdContainerType
 import de.kairos.fhir.centraxx.metamodel.enums.GenderType
 
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.ID_CONTAINER_TYPE
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.PSN
+import static de.kairos.fhir.centraxx.metamodel.PatientMaster.GENDER_TYPE
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.patientMasterDataAnonymous
 
 /**
  * Represented by a CXX PatientMasterDataAnonymous
  * @author Mike Wähnert
- * @since CXX.v.3.17.0.2
+ * @since v.1.5.0, CXX.v.3.17.1.5
  */
 patient {
 
-  id = "Patient/" + context.source["patientcontainer.id"]
+  id = "Patient/" + context.source[patientMasterDataAnonymous().patientContainer().id()]
 
-  final def idContainer = context.source["patientcontainer.idContainer"]?.find {
-    "SID" == it["idContainerType"]?.getAt("code")
+  final def idContainer = context.source[patientMasterDataAnonymous().patientContainer().idContainer()]?.find {
+    "SID" == it[ID_CONTAINER_TYPE]?.getAt(IdContainerType.CODE)
   }
 
   if (idContainer) {
     identifier {
-      value = idContainer["psn"]
+      value = idContainer[PSN]
       type {
         coding {
           system = "urn:centraxx"
-          code = idContainer["idContainerType"]?.getAt("code")
+          code = idContainer[ID_CONTAINER_TYPE]?.getAt(IdContainerType.CODE)
         }
       }
     }
   }
-  if (context.source["genderType"]) {
-    gender = mapGender(context.source["genderType"] as GenderType)
+  if (context.source[GENDER_TYPE]) {
+    gender = mapGender(context.source[GENDER_TYPE] as GenderType)
   }
-  birthDate = normalizeDate(context.source["birthdate.date"] as String)
-  deceasedDateTime = "UNKNOWN" != context.source["dateOfDeath.precision"] ? context.source["dateOfDeath.date"] : null
+
+  birthDate = normalizeDate(context.source[patientMasterDataAnonymous().birthdate().date()] as String)
+
+  deceasedDateTime = "UNKNOWN" != context.source[patientMasterDataAnonymous().dateOfDeath().precision()] ?
+      context.source[patientMasterDataAnonymous().dateOfDeath().date()] : null
+
   generalPractitioner {
     identifier {
       value = "Hannover"
@@ -39,17 +49,13 @@ patient {
   }
 
 }
- 
+
 static def mapGender(final GenderType genderType) {
   switch (genderType) {
-    case GenderType.MALE:
-      return "male"
-    case GenderType.FEMALE:
-      return "female"
-    case GenderType.UNKNOWN:
-      return "unknown"
-    default:
-      return "other"
+    case GenderType.MALE: return "male"
+    case GenderType.FEMALE: return "female"
+    case GenderType.UNKNOWN: return "unknown"
+    default: return "other"
   }
 }
 
