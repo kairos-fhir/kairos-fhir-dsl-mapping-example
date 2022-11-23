e320531d1e7a7b4910ab5f6a797b1852d15fc4b7
Mike Wähnert <mike.waehnert@kairos.de>
2022-11-23 13:16:12 +0100

    CENTRAXX-15886: extend hannover for new crfTemplateFields and extend with optiongroups/enumerations

--- src/main/groovy/projects/izi/hannover/observation.groovy
+++ src/main/groovy/projects/izi/hannover/observation.groovy
@@ -16,11 +16,11 @@ import static de.kairos.fhir.centraxx.metamodel.AbstractCode.CODE
 import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.ID_CONTAINER_TYPE
 import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.PSN
 import static de.kairos.fhir.centraxx.metamodel.RootEntities.laborMapping
+
 /**
  * Represented by a CXX LaborMapping
  * @author Mike Wähnert
  * @since kairos-fhir-dsl.v.1.12.0, CXX.v.3.18.1.19, CXX.v.3.18.2
- * TODO: extend example for Enumerations and RadioOptionGroups
  * The first code of each component represents the LaborValue.Code in CXX. Further codes could be representations in LOINC, SNOMED-CT etc.
  * LaborValueIdContainer in CXX are just an export example, but not intended to be imported by CXX FHIR API yet.
  */
@@ -73,17 +73,20 @@ observation {
   }
 
   context.source[laborMapping().laborFinding().laborFindingLaborValues()].each { final lflv ->
-    final String laborValueCode = lflv[LaborFindingLaborValue.LABOR_VALUE]?.getAt(CODE) as String
+
+    final def laborValue = lflv[LaborFindingLaborValue.LABOR_VALUE] != null
+        ? lflv[LaborFindingLaborValue.LABOR_VALUE] // before CXX.v.2022.3.0
+        : lflv["crfTemplateField"][CrfTemplateField.LABOR_VALUE] // from CXX.v.2022.3.0
+
+    final String laborValueCode = laborValue?.getAt(CODE) as String
     if (isCimdKenzusatzdaten(laborValueCode)) {
       component {
         code {
           coding {
             system = "urn:centraxx"
-
             code = laborValueCode
           }
-
-          lflv[LaborFindingLaborValue.LABOR_VALUE]?.getAt(LaborValue.IDCONTAINERS)?.each { final idContainer ->
+          laborValue?.getAt(LaborValue.IDCONTAINERS)?.each { final idContainer ->
             coding {
               system = idContainer[ID_CONTAINER_TYPE]?.getAt(CODE)
               code = idContainer[PSN] as String
@@ -91,22 +94,52 @@ observation {
           }
         }
 
-        if (isNumeric(lflv)) {
+        if (isNumeric(laborValue)) {
           valueQuantity {
             value = lflv[LaborFindingLaborValue.NUMERIC_VALUE]
-            unit = lflv[LaborFindingLaborValue.LABOR_VALUE]?.getAt(LaborValueNumeric.UNIT)?.getAt(CODE) as String
+            unit = laborValue?.getAt(LaborValueNumeric.UNIT)?.getAt(CODE) as String
           }
-        } else if (isBoolean(lflv)) {
+        } else if (isBoolean(laborValue)) {
           valueBoolean(lflv[LaborFindingLaborValue.BOOLEAN_VALUE] as Boolean)
-        } else if (isDate(lflv)) {
+        } else if (isDate(laborValue)) {
           valueDateTime {
             date = lflv[LaborFindingLaborValue.DATE_VALUE]?.getAt(PrecisionDate.DATE)
           }
-        } else if (isTime(lflv)) {
+        } else if (isTime(laborValue)) {
           valueTime(lflv[LaborFindingLaborValue.TIME_VALUE] as String)
-        } else if (isString(lflv)) {
+        } else if (isString(laborValue)) {
           valueString(lflv[LaborFindingLaborValue.STRING_VALUE] as String)
-        } else if (isCatalog(lflv)) {
+        } else if (isEnumeration(laborValue)) {
+          valueCodeableConcept {
+            lflv[LaborFindingLaborValue.MULTI_VALUE].each { final entry ->
+              coding {
+                system = "urn:centraxx:CodeSystem/UsageEntry"
+                code = entry[CODE] as String
+              }
+            }
+            lflv[LaborFindingLaborValue.CATALOG_ENTRY_VALUE].each { final entry ->
+              coding {
+                system = "urn:centraxx:CodeSystem/ValueList-" + entry[CatalogEntry.CATALOG]?.getAt(AbstractCatalog.ID)
+                code = entry[CODE] as String
+              }
+            }
+          }
+        } else if (isOptionGroup(laborValue)) {
+          valueCodeableConcept {
+            lflv[LaborFindingLaborValue.MULTI_VALUE].each { final entry ->
+              coding {
+                system = "urn:centraxx:CodeSystem/UsageEntry"
+                code = entry[CODE] as String
+              }
+            }
+            lflv[LaborFindingLaborValue.CATALOG_ENTRY_VALUE].each { final entry ->
+              coding {
+                system = "urn:centraxx:CodeSystem/ValueList-" + entry[CatalogEntry.CATALOG]?.getAt(AbstractCatalog.ID)
+                code = entry[CODE] as String
+              }
+            }
+          }
+        } else if (isCatalog(laborValue)) {
           valueCodeableConcept {
             lflv[LaborFindingLaborValue.CATALOG_ENTRY_VALUE].each { final entry ->
               coding {
@@ -142,7 +175,7 @@ observation {
             }
           }
         } else {
-          final String msg = lflv[LaborFindingLaborValue.LABOR_VALUE]?.getAt(LaborValue.D_TYPE) + " not implemented yet."
+          final String msg = laborValue?.getAt(LaborValue.D_TYPE) + " not implemented yet."
           System.out.println(msg)
         }
       }
@@ -150,33 +183,32 @@ observation {
   }
 }
 
-private static boolean isDTypeOf(final Object lflv, final List<LaborValueDType> types) {
-  return types.contains(lflv[LaborFindingLaborValue.LABOR_VALUE]?.getAt(LaborValue.D_TYPE) as LaborValueDType)
+private static boolean isDTypeOf(final Object laborValue, final List<LaborValueDType> types) {
+  return types.contains(laborValue?.getAt(LaborValue.D_TYPE) as LaborValueDType)
 }
 
-static boolean isBoolean(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.BOOLEAN])
+static boolean isBoolean(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.BOOLEAN])
 }
 
-static boolean isNumeric(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.INTEGER, LaborValueDType.DECIMAL, LaborValueDType.SLIDER])
+static boolean isNumeric(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.INTEGER, LaborValueDType.DECIMAL, LaborValueDType.SLIDER])
 }
 
-
-static boolean isDate(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.DATE, LaborValueDType.LONGDATE])
+static boolean isDate(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.DATE, LaborValueDType.LONGDATE])
 }
 
-static boolean isTime(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.TIME])
+static boolean isTime(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.TIME])
 }
 
-static boolean isEnumeration(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.ENUMERATION])
+static boolean isEnumeration(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.ENUMERATION])
 }
 
-static boolean isString(final Object lflv) {
-  return isDTypeOf(lflv, [LaborValueDType.STRING, LaborValueDType.LONGSTRING])
+static boolean isString(final Object laborValue) {
+  return isDTypeOf(laborValue, [LaborValueDType.STRING, LaborValueDType.LONGSTRING])
 }
 
 static boolean isCatalog(final Object lflv) {
