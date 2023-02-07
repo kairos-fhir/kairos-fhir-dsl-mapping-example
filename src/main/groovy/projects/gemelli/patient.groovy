2fadec02619d27a54892334e5879c7f4cc892b73
Mike Wähnert <mike.waehnert@kairos.de>
2023-02-07 18:31:11 +0100

    CENTRAXX-16544

---
+++ src/main/groovy/projects/gemelli/patient.groovy
@@ -0,0 +1,105 @@
+package projects.gemelli
+
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum
+import de.kairos.centraxx.fhir.r4.utils.FhirUrls
+import de.kairos.fhir.centraxx.metamodel.Country
+import de.kairos.fhir.centraxx.metamodel.Ethnicity
+import de.kairos.fhir.centraxx.metamodel.MultilingualEntry
+import de.kairos.fhir.centraxx.metamodel.PatientAddress
+import de.kairos.fhir.centraxx.metamodel.enums.GenderType
+
+import static de.kairos.fhir.centraxx.metamodel.AbstractCode.CODE
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.ID_CONTAINER_TYPE
+import static de.kairos.fhir.centraxx.metamodel.AbstractIdContainer.PSN
+import static de.kairos.fhir.centraxx.metamodel.IdContainerType.DECISIVE
+import static de.kairos.fhir.centraxx.metamodel.PatientMaster.GENDER_TYPE
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.patient
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.patientMasterDataAnonymous
+
+/**
+ * Represented by a CXX PatientMasterDataAnonymous
+ * Specified: http://www.hl7.org/fhir/us/core/StructureDefinition-us-core-patient.html
+ * @author Mike Wähnert
+ * @since v.1.13.0, CXX.v.2022.1.0
+ *
+ * Hints: Race, and birth sex are no explicit fields in CXX. If needed, specify the field and add extensions similar to ethnicity.
+ */
+patient {
+
+  id = "Patient/" + context.source[patientMasterDataAnonymous().patientContainer().id()]
+
+  meta {
+    profile "http://hl7.org/fhir/us/core/StructureDefinition/us-core-patient"
+  }
+
+  context.source[patientMasterDataAnonymous().patientContainer().idContainer()].each { final idContainer ->
+    final boolean isDecisive = idContainer[ID_CONTAINER_TYPE]?.getAt(DECISIVE)
+    if (isDecisive) {
+      identifier {
+        value = idContainer[PSN]
+        type {
+          coding {
+            system = FhirUrls.System.IdContainerType.BASE_URL
+            code = idContainer[ID_CONTAINER_TYPE]?.getAt(CODE)
+          }
+        }
+      }
+    }
+  }
+
+  if (context.source[GENDER_TYPE]) {
+    gender = mapGender(context.source[GENDER_TYPE] as GenderType)
+  }
+
+  if (context.source[patientMasterDataAnonymous().birthdate().date()]) {
+    birthDate {
+      date = context.source[patientMasterDataAnonymous().birthdate().date()]
+      precision = TemporalPrecisionEnum.YEAR.name()
+    }
+  }
+
+  deceasedDateTime = "UNKNOWN" != context.source[patientMasterDataAnonymous().dateOfDeath().precision()] ?
+      context.source[patientMasterDataAnonymous().dateOfDeath().date()] : null
+
+  context.source[patient().addresses()]?.each { final ad ->
+    address {
+      type = "physical"
+      city = ad[PatientAddress.CITY]
+      postalCode = ad[PatientAddress.ZIPCODE]
+      country = ad[PatientAddress.COUNTRY]?.getAt(Country.ISO2_CODE)
+      final def lineString = getLineString(ad as Map)
+      if (lineString) {
+        line lineString
+      }
+    }
+  }
+
+  final def firstEthnicity = context.source[patient().patientContainer().ethnicities()].find { final def ethnicity -> ethnicity != null }
+  if (firstEthnicity != null) {
+    extension {
+      url = "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity"
+      extension {
+        url = "text"
+        valueString = firstEthnicity[Ethnicity.NAME_MULTILINGUAL_ENTRIES].find { final def me ->
+          me[MultilingualEntry.LANG] == "de"
+        }?.getAt(MultilingualEntry.VALUE) as String
+      }
+    }
+  }
+}
+
+
+static String getLineString(final Map address) {
+  final def keys = [PatientAddress.STREET, PatientAddress.STREETNO]
+  final def addressParts = keys.collect { return address[it] }.findAll()
+  return addressParts.findAll() ? addressParts.join(" ") : null
+}
+
+static def mapGender(final GenderType genderType) {
+  switch (genderType) {
+    case GenderType.MALE: return "male"
+    case GenderType.FEMALE: return "female"
+    case GenderType.UNKNOWN: return "unknown"
+    default: return "other"
+  }
+}
