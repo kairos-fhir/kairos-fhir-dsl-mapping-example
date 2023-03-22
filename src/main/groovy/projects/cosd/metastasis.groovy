f06a63c93c9267be3b5462fca0b67f7bb4f7adb7
Mike Wähnert <mike.waehnert@kairos.de>
2023-03-22 15:42:46 +0100

    CENTRAXX-16764 FHIR custom export COSD: metastasis

---
+++ src/main/groovy/projects/cosd/metastasis.groovy
@@ -0,0 +1,72 @@
+package projects.cosd
+
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum
+import org.hl7.fhir.r4.model.Observation
+
+import static de.kairos.fhir.centraxx.metamodel.MultilingualEntry.LANG
+import static de.kairos.fhir.centraxx.metamodel.MultilingualEntry.VALUE
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.metastasis
+/**
+ * Represented by a CXX Metastasis
+ * Specified by https://simplifier.net/oncology/fernmetastasen-duplicate-2
+ * hints:
+ *  Reference to focus condition has been added additionally, because a reverse reference is not possible yet.
+ *
+ * @author Mike Wähnert
+ * @since CXX.v.3.18.1.21, CXX.v.3.18.2
+ */
+observation {
+
+  id = "Observation/Metastasis-" + context.source[metastasis().id()]
+
+  status = Observation.ObservationStatus.UNKNOWN
+
+  category {
+    coding {
+      system = "http://hl7.org/fhir/observation-category"
+      code = "laboratory"
+    }
+  }
+
+  code {
+    coding {
+      system = "http://loinc.org"
+      code = "21907-1"
+    }
+  }
+
+  subject {
+    reference = "Patient/" + context.source[metastasis().patientContainer().id()]
+  }
+
+  if (context.source[metastasis().episode()]) {
+    encounter {
+      reference = "Encounter/" + context.source[metastasis().episode().id()]
+    }
+  }
+
+  if (context.source[metastasis().date()]) {
+    effectiveDateTime {
+      date = context.source[metastasis().date()]
+      precision = TemporalPrecisionEnum.DAY.toString()
+    }
+  }
+
+  if (context.source[metastasis().localisationCodeDict()]) {
+    bodySite {
+      coding {
+        system = "cosd:localisation"
+        code = (context.source[metastasis().localisationCodeDict().code()] as String).toUpperCase()
+        display = context.source[metastasis().localisationCodeDict().nameMultilingualEntries()].find { final def me ->
+          me[LANG] == "en"
+        }?.getAt(VALUE) as String
+      }
+    }
+  }
+
+  if (context.source[metastasis().tumour()]) {
+    focus {
+      reference = "Condition/" + context.source[metastasis().tumour().centraxxDiagnosis().id()]
+    }
+  }
+}
