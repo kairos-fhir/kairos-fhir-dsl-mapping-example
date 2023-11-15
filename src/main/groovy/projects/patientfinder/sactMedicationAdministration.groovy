0ba797663649b9bd6ada334bab1f08c65f17f978
Mike Wähnert <mike.waehnert@kairos.de>
2023-11-15 14:11:32 +0100

    CENTRAXX-17773 FHIR: Transform SACT/COSD Regimen LabFindings to FHIR MedicationAdministration: comment added

--- src/main/groovy/projects/patientfinder/sactMedicationAdministration.groovy
+++ src/main/groovy/projects/patientfinder/sactMedicationAdministration.groovy
@@ -10,6 +10,7 @@ import static de.kairos.fhir.centraxx.metamodel.RootEntities.laborMapping
 
 /**
  * Transforms CXX LaborFinding of the LaborMethod "SACT_Profil" to a FHIR MedicationAdministration
+ * @since CXX.v.2023.4.1, CXX.v.2023.5.0
  */
 medicationAdministration {
 
