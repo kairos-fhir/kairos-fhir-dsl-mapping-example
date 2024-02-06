59ad395394f940d398a2214e5a9f46b831f14d9c
Jonas Küttner <jonas.kuettner@iqvia.com>
2024-02-06 18:26:46 +0100

    FNUSA: added export of Contact details phone1 and email in patient script

---
+++ src/main/groovy/customimport/patient.groovy
@@ -0,0 +1,44 @@
+package customimport
+
+import de.kairos.centraxx.fhir.r4.utils.FhirUrls
+import org.hl7.fhir.r4.model.Patient
+
+/**
+ * Transforms condition bundles.
+ * @author Mike Wähnert
+ * @since v.1.7.0, CXX.v.2022.3.0
+ */
+bundle {
+  final Patient patient = context.resource as Patient
+
+  println(patient.generalPractitioner.reference.toString())
+
+  entry {
+    fullUrl = "something"
+    resource = patient
+  }
+
+  final def sid = patient.identifier.find {
+    it.type.codingFirstRep.code == "SID"
+  }
+
+  if (sid != null) {
+    entry {
+      resource {
+        encounter {
+          id = "992"
+          identifier {
+            system = FhirUrls.System.Episode.CXX_EPISODE_ID
+            value = "New Episode Id Test"
+          }
+
+          subject {
+            identifier = sid
+          }
+
+          serviceProvider = patient.generalPractitioner[0]
+        }
+      }
+    }
+  }
+}
