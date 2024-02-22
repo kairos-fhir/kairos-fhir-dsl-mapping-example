388b1715879557201970f5c17d771655d77490d5
Jonas Küttner <jonas.kuettner@iqvia.com>
2024-02-22 18:39:20 +0100

    removed obsolete scripts and config. Added config for saving source bundles

--- src/main/groovy/customimport/patient.groovy
+++ src/main/groovy/customimport/patient.groovy
@@ -1,48 +1,53 @@
 package customimport
 
 import de.kairos.centraxx.fhir.r4.utils.FhirUrls
+import org.hl7.fhir.r4.model.Bundle
 import org.hl7.fhir.r4.model.Patient
+import org.hl7.fhir.r4.model.ResourceType
 
-/**
- * Transforms condition bundles.
- * @author Jonas Küttner
- * @since v.1.9.0, CXX.v.2024.1.0
- */
 bundle {
-  if (!context.bundleEntryComponent.hasResource()) {
-    return
-  }
 
-  final Patient patient = context.bundleEntryComponent.getResource() as Patient
+  // filter for patient entries
+  context.bundles.each {final def bundle ->
+    bundle.getEntry().findAll { it.getResource().getResourceType() == ResourceType.Patient }
+        .each {
+          final Patient patient = it.getResource() as Patient
 
-  println(patient.generalPractitioner.reference.toString())
+          // get bundle request to set to entries. Could also transform it here.
+          final Bundle.BundleEntryRequestComponent requestToSet = it.getRequest()
 
-  entry {
-    fullUrl = "something"
-    resource = patient
-  }
+          println(patient.generalPractitioner.reference.toString())
 
-  final def sid = patient.identifier.find {
-    it.type.codingFirstRep.code == "SID"
-  }
-
-  if (sid != null) {
-    entry {
-      resource {
-        encounter {
-          id = "992"
-          identifier {
-            system = FhirUrls.System.Episode.CXX_EPISODE_ID
-            value = "New Episode Id Test"
+          entry {
+            fullUrl = "something"
+            resource = patient
+            request = requestToSet
           }
 
-          subject {
-            identifier = sid
+          final def sid = patient.identifier.find {
+            it.type.codingFirstRep.code == "SID"
           }
 
-          serviceProvider = patient.generalPractitioner[0]
+          if (sid != null) {
+            entry {
+              resource {
+                encounter {
+                  id = "992"
+                  identifier {
+                    system = FhirUrls.System.Episode.CXX_EPISODE_ID
+                    value = "New Episode Id Test"
+                  }
+
+                  subject {
+                    identifier = sid
+                  }
+
+                  serviceProvider = patient.generalPractitioner[0]
+                }
+              }
+              request = requestToSet
+            }
+          }
         }
-      }
-    }
   }
 }
