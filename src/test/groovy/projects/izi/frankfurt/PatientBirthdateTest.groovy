b86b12fd1d301ddbba39bbb0f72ff6f8ec0f23b2
kairosmike <mike.waehnert@iqvia.com>
2024-03-03 19:40:37 +0100

    test fixed dsl version reverted

--- src/test/groovy/projects/izi/frankfurt/PatientBirthdateTest.groovy
+++ src/test/groovy/projects/izi/frankfurt/PatientBirthdateTest.groovy
@@ -1,11 +1,14 @@
 package projects.izi.frankfurt
 
 import common.AbstractDslBuilderTest
+import de.kairos.centraxx.fhir.r4.utils.FhirUrls
 import de.kairos.fhir.centraxx.metamodel.PatientMasterDataAnonymous
 import de.kairos.fhir.centraxx.metamodel.PrecisionDate
 import de.kairos.fhir.centraxx.metamodel.enums.DatePrecision
 import de.kairos.fhir.dsl.r4.context.Context
 import de.kairos.fhir.dsl.r4.execution.Fhir4ScriptRunner
+import org.hl7.fhir.r4.model.CodeType
+import org.hl7.fhir.r4.model.Extension
 import org.hl7.fhir.r4.model.Patient
 import org.junit.jupiter.api.Test
 
@@ -18,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue
  * Example test to run groovy mapping scripts with assumed test data.
  */
 class PatientBirthdateTest extends AbstractDslBuilderTest {
-
 
   @Test
   void testThatBirthdateIsPopulated() {
@@ -52,7 +54,9 @@ class PatientBirthdateTest extends AbstractDslBuilderTest {
     final Patient patient = (Patient) runner.run(new Context(testDataMap))
 
     // then: test your assertions
-    assertFalse(patient.hasBirthDateElement())
+    assertTrue(patient.hasBirthDateElement())
+    CodeType codeType = (CodeType) patient.getBirthDateElement().getExtensionByUrl(FhirUrls.Extension.FhirDefaults.DATA_ABSENT_REASON).getValue()
+    assertEquals("unknown",codeType.getValue())
   }
 
   static Map<String, Object> createBirthdayWithPrecisionDay() {
