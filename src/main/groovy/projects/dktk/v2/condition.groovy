ef173b4913bee4b3a68a5c6aa6664839f8133a62
kairosmike <mike.waehnert@iqvia.com>
2024-05-14 15:11:42 +0200

    CENTRAXX-19421 DKTK FHIR Cortex Mappings: (operation) procedure.complication added

--- src/main/groovy/projects/dktk/v2/condition.groovy
+++ src/main/groovy/projects/dktk/v2/condition.groovy
@@ -14,7 +14,7 @@ import static de.kairos.fhir.centraxx.metamodel.RootEntities.diagnosis
  * Specified by https://simplifier.net/oncology/primaerdiagnose
  * @author Mike Wähnert
  * @since CXX.v.3.17.1.6, v.3.17.2
- * @since CXX.v.3.18.3.X, CXX.v.2024.3.0 evidence based on the measurement profile xml/masterdata_diagnosesicherung.xml
+ * @since CXX.v.2024.3.0 evidence based on the measurement profile xml/masterdata_diagnosesicherung.xml
  */
 condition {
 
--- src/main/groovy/projects/dktk/v2/operation.groovy
+++ src/main/groovy/projects/dktk/v2/operation.groovy
@@ -14,8 +14,8 @@ import static de.kairos.fhir.centraxx.metamodel.RootEntities.surgery
  * OPS code for surgeries are not available in CXX
  * @author Mike Wähnert
  * @since CXX.v.3.17.1.6, v.3.17.2
- *
- * SurgeryComponent export is available since CXX.v.3.18.3.17, CXX.v.3.18.4,CXX.v.2023.3.9, CXX.v.2023.4.2, CXX.v.2023.5.1, CXX.v.2023.6.0
+ * @since CXX.v.3.18.3.17, CXX.v.3.18.4,CXX.v.2023.3.9, CXX.v.2023.4.2, CXX.v.2023.5.1, CXX.v.2023.6.0: SurgeryComponent export is available since
+ * @since CXX.v.2024.3.0: evidence export available based on the measurement profile xml/masterdata_diagnosesicherung.xml
  */
 procedure {
   id = "Procedure/Surgery-" + context.source[surgery().id()]
