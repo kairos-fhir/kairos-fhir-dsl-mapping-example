b16a2e4fd4be6bf7ce9e642384707b4744590812
Jonas Küttner <jonas.kuettner@iqvia.com>
2024-08-29 18:29:56 +0200

    CENTRAXX-20016
    added allergyIntolerance.groovy script
    update fhir-dsl

---
+++ src/main/groovy/projects/patientfinder/allergyIntolerance.groovy
@@ -0,0 +1,101 @@
+package projects.patientfinder
+
+import de.kairos.fhir.centraxx.metamodel.CrfTemplateField
+import de.kairos.fhir.centraxx.metamodel.LaborFindingLaborValue
+import de.kairos.fhir.centraxx.metamodel.LaborValue
+import de.kairos.fhir.centraxx.metamodel.PrecisionDate
+
+import static de.kairos.fhir.centraxx.metamodel.RecordedValue.DATE_VALUE
+import static de.kairos.fhir.centraxx.metamodel.RecordedValue.STRING_VALUE
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.laborMapping
+
+allergyIntolerance {
+
+  if (!(context.source[laborMapping().laborFinding().laborMethod().code()] as String).equals("Allergen")) {
+    return
+  }
+
+  id = "AllergyIntolerance/" + context.source[laborMapping().laborFinding().id()]
+
+  final List labFinLabVals = context.source[laborMapping().laborFinding().laborFindingLaborValues()]
+
+  final def lvClinicalStatus = findLabFindLabVal(labFinLabVals, "Allergen.Status")
+  if (lvClinicalStatus) {
+    clinicalStatus {
+      coding {
+        code = lvClinicalStatus[STRING_VALUE] as String
+      }
+    }
+  }
+
+  final def lflvCategory = findLabFindLabVal(labFinLabVals, "Allergen.Category")
+
+  if (lflvCategory) {
+    category {
+      value = lflvCategory[STRING_VALUE] as String
+    }
+  }
+
+  patient {
+    reference = "Patient/" + context.source[laborMapping().relatedPatient().id()]
+  }
+
+  final def lflvOnsetStart = findLabFindLabVal(labFinLabVals, "Start")
+  final def lflvOnsetEnd = findLabFindLabVal(labFinLabVals, "Finish")
+
+  if (lflvOnsetStart && !lflvOnsetEnd) {
+    onsetDateTime {
+      date = lflvOnsetStart[DATE_VALUE][PrecisionDate.DATE]
+    }
+  } else if (lflvOnsetStart && lflvOnsetEnd) {
+    onsetPeriod {
+      start {
+        date = lflvOnsetStart[DATE_VALUE][PrecisionDate.DATE]
+      }
+      end {
+        date = lflvOnsetEnd[DATE_VALUE][PrecisionDate.DATE]
+      }
+    }
+  }
+
+  final def lflvRecordedDate = findLabFindLabVal(labFinLabVals, "Record date")
+  if (lflvRecordedDate) {
+    recordedDate = lflvRecordedDate[DATE_VALUE]
+  }
+
+  reaction {
+    final def lflvAllergen = findLabFindLabVal(labFinLabVals, "Allergen.Allergen")
+    if (lflvAllergen) {
+      substance {
+        coding {
+          code = lflvAllergen[STRING_VALUE] as String
+        }
+      }
+    }
+
+    final def lflvRecation = findLabFindLabVal(labFinLabVals, "Allergen.Reaction")
+
+    if (lflvRecation) {
+
+      manifestation {
+        coding {
+          code = lflvRecation[STRING_VALUE] as String
+        }
+      }
+    }
+
+    final def lflvSeverity = findLabFindLabVal(labFinLabVals, "Allergen.Severity")
+
+    if (lflvSeverity) {
+      severity(lflvSeverity[STRING_VALUE] as String)
+    }
+  }
+
+
+}
+
+static def findLabFindLabVal(final List labFinLabVals, final String code) {
+  return labFinLabVals.find {
+    it[LaborFindingLaborValue.CRF_TEMPLATE_FIELD][CrfTemplateField.LABOR_VALUE][LaborValue.CODE].equals(code)
+  }
+}
